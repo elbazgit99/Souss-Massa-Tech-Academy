1f94f937ada7ca7b06883f9bc23d51e3dc817afa
mohamed essoufi <essoufimohamed18@gmail.com>
2025-05-15 16:00:00 +0100

    fix: conflict merge branches

--- backend/config/database.js
+++ backend/config/database.js
@@ -8,9 +8,9 @@ const MONGODB_URL = process.env.STRING_URI;
 export const connectDB = async () => {
     try {
         await mongoose.connect(MONGODB_URL);
-        console.log("✅ Connected to MongoDB Atlas");
+        console.log("Connected to MongoDB Atlas");
     } catch (error) {
-        console.error("❌ MongoDB connection failed:", error);
+        console.error("MongoDB connection failed:", error);
         process.exit(1); // Exit the app if connection fails
     }
 };

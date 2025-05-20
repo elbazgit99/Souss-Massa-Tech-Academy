29cf2d22615d8f498a89d5c4ed73b8ff328bcc01
Driss <driss.oudmine@gmail.com>
2025-05-20 11:49:37 +0100

    Feat: add home dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,17 +4,19 @@ import "./App.css";
 import LoginPage from "./features/layout/login";
 import RegisterPage from "./features/layout/register";
 import HomePage from "./features/pages/HomePage";
+import DashHome from "./features/layout/dash-home";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/" element={<HomePage />} />
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dash-home" element={<DashHome />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;

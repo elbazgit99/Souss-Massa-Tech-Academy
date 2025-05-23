c810f5d1136ea3a93186a46bef750491a74184af
Driss <driss.oudmine@gmail.com>
2025-05-24 00:46:00 +0100

    modify course crud

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,20 +8,20 @@ import DashHome from "./features/layout/dash-home";
 import { TableUsers } from "./components/table-users";
 
 function App() {
-    return (
-        <>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route path="/" element={<HomePage />} />
-                <Route path="/dash-home" element={<DashHome />}>
-                    <Route element={<DashHome />} />
-                    <Route path="/dash-home/users" element={<TableUsers />} />
-                    <Route path="/dash-home/courses" element={<DashHome />} />
-                </Route>
-            </Routes>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/dash-home" element={<DashHome />}>
+          <Route element={<DashHome />} />
+          <Route path="/dash-home/users" element={<TableUsers />} />
+          <Route path="/dash-home/courses" element={<DashHome />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;

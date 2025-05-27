f902bf35ec01eb610ff6d82ef9c1623351f7b712
elbazgit99 <bazness22@gmail.com>
2025-05-27 10:41:58 +0100

    feat : add dark's mode dynamic icon

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router";
-import { ThemeProvider } from "@/themes/themeProvider";
+import { ThemeProvider } from "@/components/theme-provider";
 import "./App.css";
 
 import LoginPage from "./features/layout/login";
@@ -8,14 +8,15 @@ import HomePage from "./features/pages/HomePage";
 import DashHome from "./features/layout/dash-home";
 import { TableUsers } from "./components/table-users";
 import UpdateUserForm from "./components/form-update";
-
+import { ModeToggle } from "./components/mode-toggle";
 import CoursePage from "./features/layout/CoursePage";
-import GroupsPage from "./features/layout/groupsPage"; // Updated import to the new file name and component name
+import GroupsPage from "./features/layout/groupsPage"; 
 
 function App() {
   return (
-    <>
+    
       <ThemeProvider defaultTheme="light">
+        <ModeToggle />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
@@ -32,7 +33,7 @@ function App() {
           </Route>
         </Routes>
       </ThemeProvider>
-    </>
+    
   );
 }
 
